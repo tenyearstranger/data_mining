3d1164e4553aaf65828daa3c6e9846aaad484552
mfk <21373055@buaa.edu.cn>
2023-11-21 16:33:09 +0800

    增加写入csv

--- test_dataset/__init__.py
+++ test_dataset/__init__.py
@@ -3,6 +3,7 @@
 # @Author : mfk
 # @File : __init__.py.py
 # @Software : PyCharm
+import csv
 import datetime
 import re
 
@@ -18,6 +19,7 @@ conn = pymysql.connect(
 )
 cursor = conn.cursor()
 searchTraj = 'select `entity_id`,`time`,`coordinates` from `traj` where `traj_id` = %s;'
+timeFormat = '%Y-%m-%dT%H:%M:%SZ'
 
 
 def get_lng_lat(lng_lat_string):
@@ -39,7 +41,7 @@ def getTraj(trajId):
         lng, lat = get_lng_lat(result[2])
         lngs.append(lng)
         lats.append(lat)
-        time = datetime.datetime.strptime(result[1], '%Y-%m-%dT%H:%M:%SZ')
+        time = datetime.datetime.strptime(result[1], timeFormat)
         times.append(time)
     return times, lngs, lats
 
@@ -56,21 +58,37 @@ if __name__ == "__main__":
     min_id = int(min_id[0])
 
     selectEntityId = 'select `entity_id` from `traj` where `traj_id` = %s'
-    for i in range(min_id, min_id + 5):
+    datas = []  # 最终的数据容器
+    for i in range(min_id, max_id):
         cursor.execute(selectEntityId, i)
-        entity_id = cursor.fetchone()
+        entity_id = cursor.fetchall()
+        if entity_id is None or len(entity_id) == 0:
+            continue
+        entity_id = entity_id[0]
         print("extend " + str(i) + "--------------")
         times, lngs, lats = getTraj(i)
         # 将时间转化为浮点数
-        plots = []
+        plots = []  # 插值点
         timeFloats = []
         for time in times:
             timeFloats.append(time.timestamp())
+        datas.append([entity_id, times[0], lngs[0], lats[0]])
         for j in range(1, len(timeFloats)):
-            plot = (timeFloats[j-1] + timeFloats[j])/2
-            plots.append(plot)
+            timeInterp = (timeFloats[j - 1] + timeFloats[j]) / 2
+            plots.append(timeInterp)
         # 进行插值
         res1 = np.interp(plots, timeFloats, lngs)
         res2 = np.interp(plots, timeFloats, lats)
-        print(res1)
-        print(res2)
+        # 构造写入数据
+        datas.append([entity_id, i, times[0].strftime(timeFormat), lngs[0], lats[0]])
+        for j in range(1, len(timeFloats)):
+            timeString = datetime.datetime.fromtimestamp(timeFloats[j - 1]).strftime(timeFormat)
+            datas.append([entity_id, i, timeString, res1[j - 1], res2[j - 1]])
+            datas.append([entity_id, i, times[j].strftime(timeFormat), lngs[j], lats[j]])
+        # for data in datas:
+        #     print(data)
+
+    # 写入csv
+    with open('extendedTraj.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(datas)
