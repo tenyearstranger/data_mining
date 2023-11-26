05b12c54ca54c5e757c9e500f03bd5d1a27dd07a
hzzmqzhp <2722481510@qq.com>
2023-11-26 17:17:46 +0800

    路段聚类

--- DBACAN.py
+++ DBACAN.py
@@ -83,18 +83,18 @@ def roadCluster(trajectory):  # 使用DBSCAN聚类算法进行路段划分
     # print(trajectory_sample)
     locations = np.array(trajectory_sample[['lat', 'lng']])  # 位置数据
     # print(locations)
-    param_grid = {"eps": [0.0005, 0.001, 0.003, 0.005, 0.006, 0.01],
-                  "min_samples": [6, 9, 15, 20, 30, 50, 70]
+    param_grid = {"eps": [ 0.0001, 0.001, 0.01, 0.03, 0.04, 0.045, 0.05, 0.055, 0.06, 0.07, 0.08, 0.1, 0.2, 0.5],
+                  "min_samples": [15, 30, 50, 100, 120, 140, 160, 180, 200, 220, 240, 260, 500, 1000]
                   }  # epsilon控制聚类的距离阈值，min_samples控制形成簇的最小样本数
-    dbscan = DBSCAN(eps=0.0005, min_samples=15)
-    #grid_search = GridSearchCV(estimator=dbscan, param_grid=param_grid, scoring=myScore)
-    #grid_search.fit(locations)
-    dbscan.fit(locations)
-    # grid_search.fit(trajectory_sample)
-    # print("best parameters:{}".format(grid_search.best_params_))
-    # print("label:{}".format(grid_search.best_estimator_.labels_))
-    # labels = grid_search.best_estimator_.labels_  # -1表示离群点
-    labels = dbscan.labels_
+    # dbscan = DBSCAN(eps=0.0005, min_samples=15)
+    dbscan = DBSCAN()
+    grid_search = GridSearchCV(estimator=dbscan, param_grid=param_grid, scoring=myScore)
+    grid_search.fit(locations)
+    # dbscan.fit(locations)
+    print("best parameters:{}".format(grid_search.best_params_))
+    print("label:{}".format(grid_search.best_estimator_.labels_))
+    labels = grid_search.best_estimator_.labels_  # -1表示离群点
+    # labels = dbscan.labels_
     print(labels)  # 如果你在运行DBSCAN后得到的所有标签都是-1，那就意味着你的数据集中不存在可以被聚类到其他簇的点，所有点都被视为噪声点。
     score = silhouette_score(locations, labels, metric='euclidean')  # 轮廓系数
     total_cluster = labels.max() - labels.min() + 1
@@ -129,13 +129,22 @@ if __name__ == "__main__":
     """
     cwd = pathlib.Path.cwd()
     road_data = pd.read_csv(cwd / 'extendedData' / 'node_lng_lat_only.csv', index_col=0)
+    # traj_date = pd.read_csv(cwd / 'extendedData' / 'traj_lng_lat_only.csv', index_col=0)
     # print(road_data)
-    # print(road_data.columns)
+    # print(traj_date)
     """
     # 计算缺失值
     missing_values = road_data.isna()
     total_missing_values = missing_values.sum()
     print(total_missing_values)  # 输出缺失值
     """
-    road_data_cluster, road_example = trajectoryCluster(road_data, 4232) # k = num / 4
-    roadCluster(road_example)  # 进行DBSCAN聚类
+    road_data_cluster, road_example = trajectoryCluster(road_data, 4232)  # k = num / 4, 进行k-means聚类
+    # roadCluster(road_example)  # 进行DBSCAN聚类
+    # roadCluster(road_data)
+
+    # traj_data_cluster, traj_example = trajectoryCluster(traj_date, 10000)  # k = 414844 / 4
+    # roadCluster(traj_example)  # 进行DBSCAN聚类
+
+    # 网格法：将数据空间划分为一定大小的网格，例如10x10的网格。
+    # 然后统计每个网格中的数据点数量，选择eps为一个网格的边长，min_samples为一个网格中的最小数据点数量。
+    # 这种方法可以确保算法在具有相似密度的区域进行聚类
